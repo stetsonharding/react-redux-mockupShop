cc894466e6e9e58375db128b662bd0c62c20666b
Stetson Harding <stetson2001@msn.com>
2021-09-22 20:10:42 -0700

    fetched shopping products display products on page

--- src/components/ProductComponent.js
+++ src/components/ProductComponent.js
@@ -6,16 +6,16 @@ const ProductComponent = () => {
 
   const renderList = products.map((product) => {
     return (
-      <div key={product.id} className="four column wide">
+      <div key={product.id} className="four wide column">
         <div className="ui link cards">
-          <div className="card">
+          <div className="card" style={{ height: "600px" }}>
             <div className="image">
               <img src={product.image} alt={product.title} />
             </div>
             <div className="content">
               <div className="header">{product.title}</div>
-              <div classname="meta price">$ {product.price}</div>
-              <div classname="meta">{product.category}</div>
+              <div className="meta price">$ {product.price}</div>
+              <div className="meta">{product.category}</div>
             </div>
           </div>
         </div>
